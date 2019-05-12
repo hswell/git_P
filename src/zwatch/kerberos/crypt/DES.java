d8df225dfc04e63c8ed3a65ad3d9d41b87f89393
zwatch <2690849864@qq.com>
2019-05-12 14:59:57 +0800

    Revert "fix bug"
    This reverts commit 6a7a4bc76ef955f80977dd9751e4a81e86114433.

--- src/zwatch/kerberos/crypt/DES.java
+++ src/zwatch/kerberos/crypt/DES.java
@@ -18,21 +18,16 @@ public class DES {
         keyFactory = SecretKeyFactory.getInstance("DES");
         cipher = Cipher.getInstance("DES");
     }
-    public static byte[] decrypt(byte[] src, String password) throws InvalidKeyException, InvalidKeySpecException, BadPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, NoSuchPaddingException, ShortBufferException {
+    public static byte[] decrypt(byte[] src, String password) throws InvalidKeyException, InvalidKeySpecException, BadPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, NoSuchPaddingException {
         return decrypt(src, password, true);
     }
-    public static byte[] decrypt(byte[] src, String password, boolean e) throws InvalidKeyException, InvalidKeySpecException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, ShortBufferException {
+    public static byte[] decrypt(byte[] src, String password, boolean e) throws InvalidKeyException, InvalidKeySpecException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException {
         if(keyFactory== null || cipher == null){
             init();
         }
         DESKeySpec desKey = new DESKeySpec(password.getBytes());
         SecretKey securekey = keyFactory.generateSecret(desKey);
-
         cipher.init(e?Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, securekey, random);
-        byte[] ret=new byte[8*(src.length/8)];
-        for(int i=0; i<(src.length+7)/8; i++){
-            cipher.doFinal(src, i*8, 8, ret, i*8);
-        }
         return cipher.doFinal(src);
     }
 }
